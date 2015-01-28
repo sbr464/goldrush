482c7297f1af0ed30196318c8d72a5b0f0b51dd3
Stephen Richardson <sbr464@gmail.com>
2015-01-28 14:40:40 -0700

    updates

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 $(document).on('ready', function() {
-  
+
+var windowHeight = $('.mainContainer').height();
+var windowWidth = $('.mainContainer').width();
+
+var pinPointY = $('img.marker').height();
+var pinPointX = .5 * $('img.marker').width();
+
+var currentMarkerLeft;
+var currentMarkerTop;
+
+  	$(window).resize(function(){
+  		windowHeight = $('.mainContainer').height();
+  		windowWidth = $('.mainContainer').width();
+  	});
+
+
+
+
+  	$('.mainContainer').click(function(){
+  		  	$('img.marker').click(function(){
+  		$(this).css('display', 'none');
+  	});
+  		currentMarkerLeft = (event.pageX - pinPointX) / windowWidth * 100;
+  		currentMarkerTop = (event.pageY - pinPointY) / windowHeight * 100;
+		$(this).append('<img class="marker" src="img/marker.png" style="left: ' + currentMarkerLeft +  '%; top: ' + currentMarkerTop + '%;">');
+  	});
+
+
+
 });
